f24b619ef219c9afb48a47424e0781b350367c15
markdenardo <mark.denardo@gmail.com>
2021-01-13 16:09:40 -0500

    2.9

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { id: 0, name: 'Arto Hellas', number: 7188888888 }
+     { id: 1, name: 'Arto Hellas', number: '040-123456' },
+    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ findPerson, setFindPerson ] = useState('')
+  const [ displayList, setDisplayList] = useState('')
 
   const addNewPerson = (event) => {
     
@@ -34,28 +39,55 @@ const App = () => {
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
+    // console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
+    // console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+
+  const handleSearchName = (event) => {
+    // console.log('52',event.target.value)
+    setFindPerson(event.target.value)
+  }
+
+ const searchPersons = (event) => {
+    
+    event.preventDefault()
+    // console.log('58',findPerson)
+    let filterName = persons.filter(e=>e.name===findPerson)
+    setDisplayList(filterName)
+    console.log(displayList)
+    
+ }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <form onSubmit={searchPersons}>
+        <div>
+          Find A Person in the Phonebook
+          <input value={findPerson} onChange={handleSearchName}/>
+        </div>
+        <button type="submit">Search</button>
+      </form>
+
       <form onSubmit={addNewPerson}>
         <div>name: <input value={newName} onChange={handleNameChange}/></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+          <button type="submit">add</button>
         <div>
-          <button 
-          type="submit">add</button>
+        
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map(person=><li key={person.id}>{person.name} {person.number}</li>)}
+      
+      {/* {displayList} */}
     </div>
   )
 }
